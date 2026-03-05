67d613ae5d0af556b2a8f33d9cbbe4a000940e51
delichain <95465260+delichain@users.noreply.github.com>
2026-03-05 01:41:15 -0500

    Add router/index.js

---
+++ frontend/src/router/index.js
@@ -0,0 +1,61 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '../stores/user'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import('../views/Home.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/upload',
+    name: 'Upload',
+    component: () => import('../views/Upload.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/generate',
+    name: 'Generate',
+    component: () => import('../views/Generate.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/works',
+    name: 'Works',
+    component: () => import('../views/Works.vue'),
+    meta: { requiresAuth: true }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore()
+  const token = userStore.token
+  
+  if (to.meta.requiresAuth && !token) {
+    next('/login')
+  } else {
+    next()
+  }
+})
+
+export default router
